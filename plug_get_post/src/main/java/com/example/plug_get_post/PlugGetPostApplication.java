b6cf8c70f553ec4b803fa79bc3eeda99de019e3e
Pavel <strelok-p1012@mail.ru>
2024-12-08 01:54:46 +0400

    Initial commit

---
+++ plug_get_post/src/main/java/com/example/plug_get_post/PlugGetPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.plug_get_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugGetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugGetPostApplication.class, args);
+	}
+
+}
