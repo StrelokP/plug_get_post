df901510807312f9ab1a6da3668d731b34fcc421
Pavel <strelok-p1012@mail.ru>
2024-12-20 22:10:26 +0400

    Dockerfile_V2

--- plug_get_post/src/main/java/com/example/plug_get_post/Login.java
+++ plug_get_post/src/main/java/com/example/plug_get_post/Login.java
@@ -1,10 +1,6 @@
 package com.example.plug_get_post;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.validation.constraints.NotBlank;
-
 import javax.validation.constraints.Size;
 import java.util.Random;
 
